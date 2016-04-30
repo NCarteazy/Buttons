b81a75d51238a46d7a2cb876ade2bbef569a36d1
NCarteazy <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-04-30 22:21:02 +0000

    added index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(requres, response) {
+response.sendfile(__dirname + '/index.html')
+})
+
+var port = process.env.PORT || 8080
+
+app.listen(port, function() {
+	console.log("Node app is running at:" + port)
+})
