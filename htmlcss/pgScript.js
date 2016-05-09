e0a56fa9792efcd295aec7f14073d937d4f7eee7
NCarteazy <carter.nicholas.r@gmail.com>
2016-05-09 13:08:58 -0700

    more changes

--- htmlcss/pgScript.js
+++ htmlcss/pgScript.js
@@ -1,13 +1,3 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-	host : 'localhost',
-	user : 'root',
-	password : 'nick172',
-	database : 'cmpe_172_game'
-});
-
-connection.connect();
-
 function Timer() {
 	var display = document.getElementById("timer"),
 		now = new Date(),
@@ -15,43 +5,71 @@ function Timer() {
 		endSec = (curSec < 30) ? 30 - curSec : 60 - curSec,
 		finSec = (endSec < 10) ? "0" + endSec : endSec;
 	display.innerHTML = "00:" + finSec;
+	if(1 === endSec) {
+		setTimeout(ender, 1000);
+	}
 	setTimeout(Timer, 1000);
+}
+
+function ender() {
 	
+	var button1 = document.getElementById("counter1"),
+		button2 = document.getElementById("counter2"),
+		wintext = document.getElementById("wintext"),
+		fval = Math.random();
+	if(button2.disables === true) {
+		fval = fval + 0.25;
+	}
+	if(button1.disables === true) {
+		fval = fval - 0.25;
+	}
+	
+	if(Math.random() < 0.5) {
+		winner = button1;
+	}
+	else {
+		winner = button2;
+	}
+	
+	winner.style.backgroundColor = "yellow";
+	wintext.style.display = "visible";
+	wintext.innerHTML = "WINNER";
+	setTimeout(resetPage, 750);
+}
+
+function resetPage() {
+	var button1 = document.getElementById("counter1"),
+		button2 = document.getElementById("counter2"),
+		wintext = document.getElementById("wintext");
+	button1.style.backgroundColor = "green";
+	button2.style.backgroundColor = "red";
+	button1.disables = false;
+	button2.disables = false;
+	button1.addEventListener("click", b1Click, false);
+	button2.addEventListener("click", b2Click, false); 
+	wintext.style.display = "hidden";
 	
 }
 
 function b1Click() {
-	button2.disables = true;
-	button2.style.backgroundColor = "#A08080";
-	connection.query('SELECT points FROM score WHERE color = 'green'', function (err, rows, fields) {
-		if(err) throw err;
-		var curpt = rows[0].points + 1;
-		connection.query('UPDATE score SET points = ' + curpt + ' WHERE name = 'green'', function(err, rows, fields) {
-			if(err) throw err;
-		});  
-	}	
+	var button1 = document.getElementById("counter1"),
+		button2 = document.getElementById("counter2");
+	if(button1.disables === false) {
+		button2.disables = true;
+		button2.style.backgroundColor = "#A08080";	
+	}
 }
 
 function b2Click() {
-        button1.disables = true;
-        button1.style.backgroundColor = "#A08080";
-	connection.query('SELECT points FROM score WHERE color = 'green'', function (err, rows, fields) {
-                if(err) throw err;
-                var curpt = rows[0].points + 1;
-                connection.query('UPDATE score SET points = ' + curpt + ' WHERE name = 'green'', function(err, rows, fields) {
-                        if(err) throw err;
-                });
-        }
-
+	var button1 = document.getElementById("counter1"),
+		button2 = document.getElementById("counter2");
+	if(button2.disables === false) {
+		button1.disables = true;
+		button1.style.backgroundColor = "#A08080";
+	}
 }
-
 
 window.onload = function () {
     Timer();
-	var button1 = document.getElementById("counter1"),
-		button2 = document.getElementById("counter2");
-	button1.addEventListener("click", b1Click, false);
-	button2.addEventListener("click", b2Click, false); 
+	resetPage(); 
 };
-
-connection.end();
