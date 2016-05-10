0c858f999a33927a31e31f6fcbcf6c894bb9edaf
NCarteazy <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-05-10 07:00:39 +0000

    again

--- htmlcss/pgScript.js
+++ htmlcss/pgScript.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
   $('#counter1').on('click', function(e){
     e.preventDefault();
     $.ajax({
-      url:  "https://api.mlab.com/api/1/databases/cmpe172game/collections/score?apiKey=zSElocti0xgz1UhZKYD9ezXcaMO7BFqq"
+      url:  "https://api.mlab.com/api/1/databases/cmpe172game/collections/score?f={"green": 1}&apiKey=zSElocti0xgz1UhZKYD9ezXcaMO7BFqq"
     }).done(function(data){
-        console.log(data.green);
+        console.log(data);
       });
   });
 
