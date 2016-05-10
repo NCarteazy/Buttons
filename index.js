5a28c170a23d793f49e0eb8953d3ab6427c637d5
NCarteazy <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-05-10 02:57:38 +0000

    try

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var express = require('express')
 var app = express()
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://carteazy:xb0xlive@ds051923.mlab.com:51923/cmpe172game';
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("Connected correctly to server.");
+	db.close();
+});
+
+
 app.use(express.static(__dirname + '/htmlcss'))
 
 app.get('/', function(requres, response) {
