e7b58de29bfeab866b4467bd1d337ba54396a764
NCarteazy <ubuntu@ip-172-31-45-130.us-west-2.compute.internal>
2016-05-10 00:53:50 +0000

    pls

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ response.sendfile(__dirname + 'index.html')
 var port = process.env.PORT || 8080
 
 app.listen(port, function() {
+	console.log("DIR: " + __dirname)
 	console.log("Node app is running at:" + port)
 })
